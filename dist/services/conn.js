f915be958dc91521f26e53e50217859ba7a689c4
JohnDias0 <dias44520@gmail.com>
2023-10-24 19:27:00 -0300

    Reparo da package.json do boopeep-server

--- dist/services/conn.js
+++ dist/services/conn.js
@@ -1,1 +1,12 @@
 "use strict";
+const mongoose = require('mongoose');
+async function main() {
+    try {
+        mongoose.set("strictQuery", true);
+        await mongoose.connect("mongodb+srv://root:<LTittBNnYnl3LSYi>@cluster0.wofeoxy.mongodb.net/?retryWrites=true&w=majority");
+    }
+    catch (error) {
+        console.log(`Erro: ${error}`);
+    }
+}
+module.exports = main;
