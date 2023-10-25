9a705cb78ff4e12565d5ace911122248c780967c
JohnDias0 <dias44520@gmail.com>
2023-10-24 22:34:29 -0300

    mudanças.

--- src/services/server.ts
+++ src/services/server.ts
@@ -1,13 +1,15 @@
 import { Request, Response } from 'express';
 import routes from '../routes/routes';
+import header from ''
 import cors from 'cors'
 const express = require('express')
 const app = express();
 const port = 4000;
 
+
 app.use(express.json());
 app.use(routes);
-app.use(cors()); // Adicione parênteses para chamar a função cors
+app.use(cors(corsOptions));
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World');
@@ -16,3 +18,8 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
     console.log('Listening on port 4000');
 });
+
+
+
+
+
