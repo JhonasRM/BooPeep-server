f8baeacb778375ed89ed85f8da96115852200e91
John Dias <120695464+JohnDias0@users.noreply.github.com>
2023-10-25 22:37:09 -0300

    Update server.ts

--- src/services/server.ts
+++ src/services/server.ts
@@ -1,25 +1,22 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import routes from '../routes/routes';
-import header from ''
-import cors from 'cors'
+
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = 4000;
 
 
 app.use(express.json());
 app.use(routes);
-app.use(cors(corsOptions));
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World');
-});
+app.use(cors())
+app.use(function(req: Request, res: Response, next: NextFunction) {
+  res.header("Access-Control-Allow-Origin", "https://localhost:5173"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+})
 
 app.listen(port, () => {
     console.log('Listening on port 4000');
 });
-
-
-
-
 
